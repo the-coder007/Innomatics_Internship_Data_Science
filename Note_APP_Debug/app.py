6c8588f20acf37fbbb508fe614c15cc38d2ee285
MRITUNJAY SHAWARN <109850287+the-coder007@users.noreply.github.com>
2023-03-25 17:58:57 +0530

    Add files via upload

---
+++ Note_APP_Debug/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+@app.route('/', methods=["GET","POST"])
+def index():
+    note = request.form.get("note")
+    notes.append(note)
+    return render_template("home.html", notes=notes)
+
+@app.route('/result')
+def result():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
