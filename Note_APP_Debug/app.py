13047d0f73775a944266b6039a89f6714b5621fb
MRITUNJAY SHAWARN <109850287+the-coder007@users.noreply.github.com>
2023-03-25 18:30:20 +0530

    Update app.py

--- Note_APP_Debug/app.py
+++ Note_APP_Debug/app.py
@@ -9,10 +9,6 @@ def index():
     notes.append(note)
     return render_template("home.html", notes=notes)
 
-@app.route('/result')
-def result():
-    return render_template('home.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
